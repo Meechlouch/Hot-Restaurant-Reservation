e60cc7edbe87c8c58c893ad3ea72e1065485a481
phillipmerriman <phillipmerriman@gmail.com>
2020-12-10 20:02:37 -0600

    added dem routes

--- server.js
+++ server.js
@@ -10,7 +10,20 @@ app.use(express.json());
 const reservations = [];
 const waitList = [];
 
+//routes
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+})
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+})
+
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+})
+
 //listener
 app.listen(PORT, function(){
-    console.log("App is listening on PORT " + PORT);
+    console.log("App is listening at http://localhost:" + PORT);
 });
